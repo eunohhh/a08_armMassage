dc74617616e96d5932ed5e982f975ab7521ce080
Eun Oh <bdohhhhh@gmail.com>
2024-06-04 22:35:50 +0900

    240604 [update] : my page protected route update

--- src/pages/PersonalPage.jsx
+++ src/pages/PersonalPage.jsx
@@ -1,11 +1,6 @@
 import MyPage from '@/components/My/MyPage';
-import useAuth from '@/hooks/useAuth';
-import { Navigate } from 'react-router-dom';
 
 function PersonalPage() {
-    const { user } = useAuth();
-
-    if (!user) return <Navigate to="/" />;
     return <MyPage />;
 }
 
