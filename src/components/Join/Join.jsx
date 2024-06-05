2d4ceeda3c949c2884141263e768cf3053911ac8
airtrader <hosi2714@naver.com>
2024-06-05 14:32:19 +0900

    CSS fix

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -14,7 +14,7 @@ const Join = () => {
     const navigate = useNavigate();
 
     //이메일 정규식
-    const emailRegEx = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    const emailRegEx = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/gi;
 
     const emailCheck = (userEmail) => {
         return emailRegEx.test(userEmail); //형식에 맞을 경우, true 리턴
